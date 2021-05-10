9a3c05621df07914dff4c96363611462dc9605ee
unknown <radoslawkrolikowski4@gmail.com>
2021-05-10 15:55:58 +0100

    Add MariaDB and Kafka configuration for Docker

--- config.py
+++ config.py
@@ -2,9 +2,15 @@
 
 """
 
-mariadb_hostname = 'localhost'
+# If using Docker use the mariadb_hostname = 'mariadb', otherwise use 'localhost'
+mariadb_hostname = 'localhost' # 'mariadb'
 mariadb_port = 3306
 mariadb_database_name = 'adsb'
 mariadb_table_name = 'adsb'
+mariadb_user = '<user_name>'
+mariadb_password = '<your_password>'
 
 kafka_config = {'servers': ['localhost:9092'], 'topics': ['adsb', 'adsb-pred']}
+
+# For Docker use the following Kafka configuration (uncomment)
+# kafka_config = {'servers': ['kafka:9092'], 'topics': ['adsb', 'adsb-pred']}
